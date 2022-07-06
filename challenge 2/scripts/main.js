4c7ddd5c81cb2610f328588f43c3d2557006af32
jSalern0 <salerno.jullya@hotmail.com>
2022-07-06 23:46:38 +0200

    fixing error in timezone

--- challenge 2/scripts/main.js
+++ challenge 2/scripts/main.js
@@ -13,25 +13,31 @@ var london = document.getElementsByClassName('london')[0]
 var tokyo = document.getElementsByClassName('tokyo')[0]
 var beijing = document.getElementsByClassName('beijing')[0]
 
-const getHours = (fuso, sinal, clock) => {//arrow function
-
-  const date = new Date()
-  const hours = sinal === '+' ? date.getHours() + fuso : date.getHours() - fuso //
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
-  const hour = hours < 10 ? `0${hours}` : hours //? pergunta se a hora e menos q 10, se verdade insere a 0 em frente da hora
-  const minute = minutes < 10 ? `0${minutes}` : minutes//
-  const second = seconds < 10 ? `0${seconds}` : seconds
-  const amPm = hours < 12 ? ' AM' : ' PM'
-  clock.innerHTML = `${hour}:${minute}:${second}${amPm}`
+const getHours = (timeZone, clock) => {//arrow function
+  const options = {
+    timeZone,
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+  const hour = new Intl.DateTimeFormat([],options);
+console.log(hour);
+  // const date = new Date()
+  // const hours = sinal === '+' ? date.getHours() + fuso : date.getHours() - fuso //
+  // const minutes = date.getMinutes()
+  // const seconds = date.getSeconds()
+  // const hour = hours < 10 ? `0${hours}` : hours //? pergunta se a hora e menos q 10, se verdade insere a 0 em frente da hora
+  // const minute = minutes < 10 ? `0${minutes}` : minutes//
+  // const second = seconds < 10 ? `0${seconds}` : seconds
+  // const amPm = hours < 12 ? ' AM' : ' PM'
+  clock.innerHTML = hour.format(new Date())
 }
 
 setInterval(() => {
-  getHours(0, '+', clockAms)
-  getHours(5, '-', clockSp)
-  getHours(1, '-', clockLdn)
-  getHours(7, '+', clockTky)
-  getHours(6, '+', clockBjg)
+  getHours('Europe/Amsterdam',clockAms)
+  getHours('America/Sao_Paulo', clockSp)
+  getHours('Europe/London', clockLdn)
+  getHours('Asia/Tokyo', clockTky)
+  getHours('Asia/Shanghai', clockBjg)
 }, 1000)
 
 bluePlanet.addEventListener('click', () => {
