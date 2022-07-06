546722648f46a027a9467c21ff4c4f5bd93286ef
jSalern0 <salerno.jullya@hotmail.com>
2022-07-06 19:59:05 +0200

    Fixing error of closing
    Map is now showing

--- challenge 3/scripts/main.js
+++ challenge 3/scripts/main.js
@@ -1,6 +1,6 @@
 // Set api token
 mapboxgl.accessToken = 'pk.eyJ1IjoianVsbHlhcyIsImEiOiJjbDU4Mzk5cG8wZ3V4M2R1a3BzN3p3N3lvIn0.nZ1jceZkUYDgRT9qKTIDDg';
-
+console.log(mapboxgl);
 // building map
 const map = new mapboxgl.Map({
   container: 'map',
@@ -53,5 +53,84 @@ map.on('click', (event) => {
         'circle-color': '#f30'
       }
     });
+}
+
+});
+
+async function getRoute(end) {
+  // the start will always be the same, only the end or destination will change
+  const query = await fetch(
+    `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+    { method: 'GET' }
+  );
+  const json = await query.json();
+  const data = json.routes[0];
+  const route = data.geometry.coordinates;
+  const geojson = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: route
+    }
+  };
+  // if the route already exists on the map, we'll reset it using setData
+  if (map.getSource('route')) {
+    map.getSource('route').setData(geojson);
+  }
+  // otherwise, we'll make a new request
+  else {
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geojson
+      },
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#3887be',
+        'line-width': 5,
+        'line-opacity': 0.75
+      }
+    });
   }
 }
+
+map.on('load', () => {
+  // make an initial directions request that
+  // starts and ends at the same location
+  getRoute(start);
+
+  // Add starting point to the map
+  map.addLayer({
+    id: 'point',
+    type: 'circle',
+    source: {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: start
+            }
+          }
+        ]
+      }
+    },
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#3887be'
+    }
+  });
+
+
+
+});
